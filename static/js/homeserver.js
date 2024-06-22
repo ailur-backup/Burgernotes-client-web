c5ee37cd9920de74cc08a24e13781ff3d73be4af
Tracker-Friendly <arzumify@noreply.concord.hectabit.org>
2024-06-22 14:58:29 +0100

    Librejs homeserver.js

--- static/js/homeserver.js
+++ static/js/homeserver.js
@@ -1,3 +1,5 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
+
 let homeserverBox = document.getElementById("homeserverBox")
 let statusBox = document.getElementById("statusBox")
 let changeButton = document.getElementById("changeButton")
@@ -52,3 +54,5 @@ changeButton.addEventListener("click", (event) => {
     }
     doStuff()
 });
+
+// @license-end
